faae709851a3139ea465a42f3cbcc03ce49a0676
mingki831 <mingki831@naver.com>
2022-09-21 16:12:02 +0900

    footer

--- app.py
+++ app.py
@@ -9,16 +9,12 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.wtjymgq.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
-<<<<<<< HEAD
 # name = '산'
 # post = db.post_content.find({'group_name':'/.*'+"산"+'.*/'})
 #
 # p_name = [p['group_name'] for p in post]
 #
 # print(p_name)
-
-=======
->>>>>>> e3eed63def5b3b378a6f44106316f64058336043
 
 # test_user = {
 #     'id':'sehun@naver.com',
@@ -35,14 +31,10 @@ db = client.dbsparta
 # 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
 SECRET_KEY = 'TEAM'
 
-<<<<<<< HEAD
-=======
 # JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
 # 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
 SECRET_KEY = 'TEAM'
 
-
->>>>>>> e3eed63def5b3b378a6f44106316f64058336043
 # member_doc = {'id':'wlstpgns51@naver.com', 'pwd':'wls124578' }
 # db.member.insert_one(member_doc)
 # member = list(db.member.find({},{'_id':False}))
@@ -62,16 +54,12 @@ SECRET_KEY = 'TEAM'
 
 @app.route('/')
 def home():
-<<<<<<< HEAD
 
    return render_template('login.html')
 
 @app.route('/MainPage')
 def main_page():
-
-   return render_template('MainPage.html')
-
-=======
+    return render_template('MainPage.html')
 
     cookie_name = 'test'
     resp = make_response(render_template('MainPage.html'))
@@ -79,7 +67,7 @@ def main_page():
 
     return resp
     return render_template('MainPage.html')
->>>>>>> e3eed63def5b3b378a6f44106316f64058336043
+
 
 @app.route('/udongdong/posts', methods=["GET"])
 def posts_list():
@@ -229,6 +217,7 @@ def api_login():
     # 찾지 못하면
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
 
 
 
